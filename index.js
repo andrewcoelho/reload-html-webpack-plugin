8dff0cbe944ae6b6d15a4c1db3c36da7c816ae0d
Andrew Coelho <acoelho.baa@gmail.com>
2016-08-12 15:37:04 -0700

    Fix port numbers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var io = require('socket.io')(8196);
 
 const newConnection = function() {
-    const socket = io.connect('http://localhost:8096/');
+    const socket = io.connect('http://localhost:8196/');
 
     socket.on('reload', function() {
         window.location.reload();
@@ -13,7 +13,7 @@ function ReloadPlugin() {};
 ReloadPlugin.prototype.apply = function(compiler) {
     compiler.plugin('compilation', function(compilation) {
         compilation.plugin('html-webpack-plugin-before-html-processing', function(htmlPluginData, callback) {
-            htmlPluginData.html += '<script src="http://localhost:8096/socket.io/socket.io.js"></script>';
+            htmlPluginData.html += '<script src="http://localhost:8196/socket.io/socket.io.js"></script>';
             htmlPluginData.html += '<script>var socket = io.connect("http://localhost:8196");socket.on("reload", function(){window.location.reload()});</script>'
             callback(null, htmlPluginData);
         });
