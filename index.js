f43ba60c3f3d98f386403e7951d66422eb66dcc8
Anagani Sai Kiran <saikirananagani7@gmail.com>
2018-03-28 14:56:45 +0530

    fix line endings

--- index.js
+++ index.js
@@ -12,22 +12,15 @@ ReloadPlugin.prototype.apply = function (compiler) {
   };
 
   compiler.hooks.compilation.tap('ReloadPlugin', (compilation) => {
-    
-compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tap('ReloadPlugin', 
-(data) => {
-      data.html += '<script 
-src="http://localhost:8196/socket.io/socket.io.js"></script>';
+    compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tap('ReloadPlugin', (data) => {
+      data.html += '<script src="http://localhost:8196/socket.io/socket.io.js"></script>';
       data.html +=
-        '<script>var socket = 
-io.connect("http://localhost:8196");socket.on("reload", 
-function(){window.location.reload()});</script>';
+        '<script>var socket = io.connect("http://localhost:8196");socket.on("reload", function(){window.location.reload()});</script>';
     });
-    compilation.hooks.htmlWebpackPluginAfterEmit.tap('ReloadPlugin', 
-(data) => {
+    compilation.hooks.htmlWebpackPluginAfterEmit.tap('ReloadPlugin', (data) => {
       io.emit('reload');
     });
   });
 };
 
 module.exports = ReloadPlugin;
-
